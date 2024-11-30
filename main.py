24031681c1f0deec2b5d526e8a464b254532c44b
saif-11bit <saifmdco@gmail.com>
2024-11-30 12:15:48 +0530

    added requirements.txt file

--- main.py
+++ main.py
@@ -45,31 +45,5 @@ def segment_image():
 
 
 
-
-    # # Threshold the mask to create a binary mask
-    # binary_mask = (mask > 0.5).astype(np.uint8)  # Adjust threshold if necessary
-    # inverse_mask = (1 - binary_mask) * 255  # Invert the mask for the background
-
-    # # Separate foreground (person) and background
-    # orig_img = result[0].orig_img
-    # background = cv2.bitwise_and(orig_img, orig_img, mask=(1 - binary_mask))
-    # foreground = cv2.bitwise_and(orig_img, orig_img, mask=binary_mask)
-
-    # # Save the layers as temporary files
-    # foreground_path = "static/foreground.png"
-    # background_path = "static/background.png"
-    # mask_path = "static/mask.png"
-    # cv2.imwrite(foreground_path, foreground)
-    # cv2.imwrite(background_path, background)
-    # cv2.imwrite(mask_path, inverse_mask)
-
-    # # Return full URLs
-    # print(url_for("static", filename="foreground.png", _external=True))
-    # return jsonify({
-    #     "foreground_url": url_for("static", filename="foreground.png", _external=True),
-    #     "background_url": url_for("static", filename="background.png", _external=True),
-    #     "inverse_mask_url": url_for("static", filename="mask.png", _external=True),
-    # })
-
 if __name__ == "__main__":
     app.run(debug=True)
